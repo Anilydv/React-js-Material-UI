5fbc755e5db352a8ab751205c332f69d6b904c40
Anilydv <sanokanxa4516@gmail.com>
2019-12-12 15:26:16 +0545

    data store

--- src/Componets/Exercises/index.js
+++ src/Componets/Exercises/index.js
@@ -1,19 +1,50 @@
-import React from "react";
-import { Grid } from "@material-ui/core/";
-import LeftPane from "./LeftPane";
-import RightPane from "./RightPane"
+import React, { Fragment } from "react";
+import {
+  Grid,
+  Paper,
+  Typography,
+  List,
+  ListItem,
+  ListItemText
+} from "@material-ui/core/";
 
 const style = {
-    Paper: { padding: 20, marginBottom: 10}
-}
+  Paper: {
+    padding: 20,
+    marginTop: 10,
+    marginBottom: 10,
+    height: 500,
+    overflowY: "auto"
+  }
+};
 
-export default props => (
+export default ({ exercises }) => (
   <Grid container sm>
     <Grid item sm>
-      <LeftPane style={style} />
+      <Paper style={style.Paper}>
+        {exercises.map(([group, exercises]) => (
+          <Fragment>
+            <Typography variant="h6" style={{ textTransform: "capitalize" }}>
+              {group}
+            </Typography>
+            <List component="nav" aria-label="secondary mailbox folders">
+              {exercises.map(({ title }) => (
+                <ListItem button>
+                  <ListItemText primary={title} />
+                </ListItem>
+              ))}
+            </List>
+          </Fragment>
+        ))}
+      </Paper>
     </Grid>
     <Grid item sm>
-      <RightPane style={style}/>
+      <Paper style={style.Paper}>
+        <Typography variant="h3">Welcome!</Typography>
+        <Typography variant="subtitle1" style={{ marginTop: 20 }}>
+          Please select the exercise from the list on the left...
+        </Typography>
+      </Paper>
     </Grid>
   </Grid>
 );
--- src/Componets/Layouts/Header.js
+++ src/Componets/Layouts/Header.js
@@ -2,19 +2,12 @@ import React from "react";
 import "./Header.css";
 import { AppBar, Toolbar, Typography } from "@material-ui/core";
 
-class Header extends React.Component {
-  render() {
-    return (
-      <div className="header-info">
-        <AppBar position="static">
-          <Toolbar className="header">
-            <Typography variant="h5" gutterBottom>
-              Exercise Database
-            </Typography>
-          </Toolbar>
-        </AppBar>
-      </div>
-    );
-  }
-}
-export default Header;
+export default props => (
+  <AppBar position="static">
+    <Toolbar className="header">
+      <Typography variant="h5">
+        Exercise Database
+      </Typography>
+    </Toolbar>
+  </AppBar>
+);
